c9d6a3eb90b3fbd290817456219f0725889ec3ef
mathisescriva <mathis.escriva@edu.ece.FR>
2025-10-28 15:59:01 +0100

    Fix: Mise à jour vidéo YouTube et correction des footers de tous les articles de blog
    - Remplacement de la vidéo YouTube par le nouveau lien (zFzSVPEj2nA)
    - Correction de l'image manquante pour le 2ème article du blog (reunions_simplifie.png)
    - Uniformisation des footers de toutes les pages blog avec la structure de la page principale
    - Correction des chemins d'images dans les footers (logo_gilbert.png)
    - Correction des caractères mal encodés dans les footers
    - Ajout des liens fonctionnels vers les pages légales et de contact

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 <iframe 
                     width="100%" 
                     height="100%" 
-                    src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1&rel=0" 
+                    src="https://www.youtube.com/embed/zFzSVPEj2nA?autoplay=1&rel=0" 
                     frameborder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
                     allowfullscreen
